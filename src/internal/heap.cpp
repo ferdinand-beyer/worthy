08520543936b2d678b52cbf77016885ad7e243e9
Ferdinand Beyer <fb@fbeyer.com>
2017-08-13 16:15:54 +0200

    Add very naive heap object allocation

--- src/internal/heap.cpp
+++ src/internal/heap.cpp
@@ -1,16 +1,40 @@
+#include "internal/check.h"
 #include "internal/heap.h"
 
 #include "internal/reference-space.h"
 
+#include <cstdlib>
+
+
 namespace worthy {
 namespace internal {
+
 
 Heap::Heap() {
     reference_space_.reset(new ReferenceSpace(this));
 }
 
+
 Heap::~Heap() {
+    for (void* memory : allocated_) {
+        std::free(memory);
+    }
+    allocated_.clear();
 }
+
+
+void* Heap::allocate(std::size_t size) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    // This is a very naive scheme used for testing only!
+    void* memory = std::malloc(size);
+    if (!memory) {
+        return nullptr;
+    }
+    auto result = allocated_.insert(memory);
+    WORTHY_CHECK(result.second);
+    return memory;
+}
+
 
 } } // namespace worthy::internal
 
--- src/internal/heap.h
+++ src/internal/heap.h
@@ -1,26 +1,49 @@
 #ifndef WORTHY_INTERNAL_HEAP_H_
 #define WORTHY_INTERNAL_HEAP_H_
 
+
 #include "internal/macros.h"
 
+#include <cstddef>
 #include <memory>
+#include <mutex>
+#include <new>
+#include <set>
+
 
 namespace worthy {
 namespace internal {
 
+
 class ReferenceSpace;
+
 
 class Heap {
 public:
     Heap();
     ~Heap();
 
+    template<typename T, typename... Args>
+    T* construct(Args&&... args) {
+        void* memory = allocate(sizeof(T));
+        if (!memory) {
+            return nullptr;
+        }
+        return new (memory) T(std::forward<Args>(args)...);
+    }
+
+    void* allocate(std::size_t size);
+
 private:
     WORTHY_DISABLE_COPY(Heap);
 
+    std::mutex mutex_;
+    std::set<void*> allocated_;
     std::unique_ptr<ReferenceSpace> reference_space_;
 };
 
+
 } } // namespace worthy::internal
+
 
 #endif // WORTHY_INTERNAL_HEAP_H_
