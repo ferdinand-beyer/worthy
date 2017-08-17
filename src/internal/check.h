1ce5923f277884f559786e6912cc5f1c11a2e0f7
Ferdinand Beyer <fb@fbeyer.com>
2017-08-17 09:11:33 +0200

    Use BOOST_UNLIKELY in checks

--- src/internal/check.h
+++ src/internal/check.h
@@ -1,14 +1,18 @@
 #ifndef WORTHY_CHECK_INTERNAL_H_
 #define WORTHY_CHECK_INTERNAL_H_
 
+
 #include "internal/macros.h"
+
+#include <boost/config.hpp>
+
 
 namespace worthy {
 namespace internal {
 
+
 void fatal(const char* file, int line, const char* format, ...);
 
-} } // namespace worthy::internal
 
 #if !defined(WORTHY_ENABLE_DEBUG_CHECKS)
     #if defined(DEBUG)
@@ -18,34 +22,46 @@ void fatal(const char* file, int line, const char* format, ...);
     #endif
 #endif
 
+
 #define WORTHY_CHECK_MSG(condition, message)                \
     do {                                                    \
-        if (!(condition)) {                                 \
+        if (BOOST_UNLIKELY(!(condition))) {                 \
             ::worthy::internal::fatal(__FILE__, __LINE__,   \
                     "Check failed: %s", message);           \
         }                                                   \
     } while (false)
 
+
 #define WORTHY_CHECK(condition)                             \
     WORTHY_CHECK_MSG(condition, #condition)
 
+
 #if WORTHY_ENABLE_DEBUG_CHECKS
+
 
 #define WORTHY_DCHECK_MSG(condition, message)               \
     do {                                                    \
-        if (!(condition)) {                                 \
+        if (BOOST_UNLIKELY(!(condition))) {                 \
             ::worthy::internal::fatal(__FILE__, __LINE__,   \
                     "Debug check failed: %s", message);     \
         }                                                   \
     } while (false)
 
+
 #define WORTHY_DCHECK(condition) \
     WORTHY_DCHECK_MSG(condition, #condition)
 
+
 #else
+
 
 #define WORTHY_DCHECK(condition) WORTHY_NOOP
 
-#endif
+
+#endif // WORTHY_ENABLE_DEBUG_CHECKS
+
+
+} } // namespace worthy::internal
+
 
 #endif // WORTHY_INTERNAL_CHECK_H_
